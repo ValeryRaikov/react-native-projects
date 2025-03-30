f636426ce11c43179c74100cddba490e48568c05
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-03-30 20:55:03 +0300

    Create feed screen

--- Instagram/src/app/(tabs)/index.tsx
+++ Instagram/src/app/(tabs)/index.tsx
@@ -1,5 +1,14 @@
-import { Text } from "react-native";
+import { View, FlatList } from "react-native";
+import posts from "~/assets/data/posts.json";
+import PostListItem from "~/src/components/PostListItem";
 
 export default function Feed() {
-    return <Text className="text-3xl font-bold">Feed</Text>;
+    return (
+        <FlatList 
+            data={posts} 
+            contentContainerStyle={{ gap: 10 }}
+            renderItem={({ item }) => <PostListItem post={item} /> }
+            showsVerticalScrollIndicator={false}
+        />
+    );
 }
