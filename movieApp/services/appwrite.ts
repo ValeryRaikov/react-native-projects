68ea5c5033dcfc9e640beba857a16b4d39f88278
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2025-04-03 16:39:31 +0300

    Add Trending Movies Card

--- movieApp/services/appwrite.ts
+++ movieApp/services/appwrite.ts
@@ -33,6 +33,19 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
     } catch (err) {
         console.log(err);
         throw err;
-        
     } 
+}
+
+export const getTrendingMovies = async (): Promise<TrendingMovie[] | undefined> => {
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.limit(5),
+            Query.orderDesc('count'),
+        ]);
+
+        return result.documents as unknown as TrendingMovie[];
+    } catch (err) {
+        console.log(err);
+        return undefined;
+    }
 }
